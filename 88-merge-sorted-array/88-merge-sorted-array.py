bf68cdfc95ef933f64aa651054776d27ad477b6f
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-03-29 01:50:08 +0900

    Time: 70 ms (18.08%), Space: 13.9 MB (42.71%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -3,8 +3,9 @@ class Solution:
         if(m+n > 0):
             if (n == 0) or (m > 0 and nums1[m-1] >= nums2[n-1]):
                 nums1[m+n-1] = nums1[m-1]
-                self.merge(nums1, m-1, nums2, n)    
+                m -= 1
+                
             else:
                 nums1[m+n-1] = nums2[n-1]
-                self.merge(nums1, m, nums2, n-1)    
-            
+                n -= 1
+            self.merge(nums1, m, nums2, n)    
