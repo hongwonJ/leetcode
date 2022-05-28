9c7e4a8f4fc0fc4ffde9b1dfdbd736fbc8bebb85
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-05-29 04:42:48 +0900

    Time: 356 ms, Memory: 23.8 MB - LeetHub

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.py
@@ -3,3 +3,14 @@ class Solution:
         rst, dict = [], {}
         for n in nums: dict[n] = True
         return [i+1 for i in range(len(nums)) if i+1 not in dict]
+
+    
+'''
+    numSet = sorted(set(nums))
+    rst, j = [], 0
+    for i in range(1, len(nums) + 1):
+        if i == numSet[j]: j += 1
+        else: rst.append(i)
+    return rst
+'''
+
