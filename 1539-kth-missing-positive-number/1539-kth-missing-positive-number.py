b5973658b915f796a1f1904de65614d734835f6f
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-06-03 01:59:53 +0900

    Time: 65 ms (60.56%), Space: 14 MB (84.50%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -3,21 +3,11 @@ class Solution:
         a, b = 0, len(arr) - 1
         while a <= b:
             c = (a + b) // 2
-            if arr[c] - (c + 1) == k:
-                while c - 1 >= 0 and arr[c - 1] == arr[c] - 1: c -= 1
-                return arr[c] - 1
-            elif arr[c] - (c + 1) < k:
-                a = c + 1
-            else:
-                b = c - 1
-        if arr[c] - (c + 1) < k:
-            while c + 1 < len(arr) and arr[c + 1] == arr[c]: c += 1
-            return arr[c] + (k - (arr[c] - (c + 1)))
-        else:
-            while c - 1 >= 0 and arr[c - 1] == arr[c]: c -= 1
-            return arr[c] - 1 - (arr[c] - (c + 1) - k)
+            if arr[c] - (c + 1) < k: a = c + 1
+            else: b = c - 1
+        return a + k
         
-            
+        
         
                 
                 
