68a87db9aab657938ea06fbe68163dc683b1b315
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-06-04 20:55:01 +0900

    Time: 4037 ms (5.00%), Space: 27.9 MB (39.26%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,15 +1,39 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        def findBestSubarray(nums, left, right):
+            # Base case - empty array.
+            if left > right:
+                return -math.inf
+
+            mid = (left + right) // 2
+            curr = best_left_sum = best_right_sum = 0
+
+            # Iterate from the middle to the beginning.
+            for i in range(mid - 1, left - 1, -1):
+                curr += nums[i]
+                best_left_sum = max(best_left_sum, curr)
+
+            # Reset curr and iterate from the middle to the end.
+            curr = 0
+            for i in range(mid + 1, right + 1):
+                curr += nums[i]
+                best_right_sum = max(best_right_sum, curr)
+
+            # The best_combined_sum uses the middle element and
+            # the best possible sum from each half.
+            best_combined_sum = nums[mid] + best_left_sum + best_right_sum
+
+            # Find the best subarray possible from both halves.
+            left_half = findBestSubarray(nums, left, mid - 1)
+            right_half = findBestSubarray(nums, mid + 1, right)
+
+            # The largest of the 3 is the answer for any given input array.
+            return max(best_combined_sum, left_half, right_half)
         
-        sum_Sub, max_Sub = 0, float('-inf')
-        
-        for i in range(len(nums)):
-            sum_Sub += nums[i]
-            if sum_Sub > max_Sub: max_Sub = sum_Sub
-            if sum_Sub < 0: sum_Sub = 0
-        
-        return max_Sub
-        
+        # Our helper function is designed to solve this problem for
+        # any array - so just call it using the entire input!
+        return findBestSubarray(nums, 0, len(nums) - 1)
+
             
                 
             
