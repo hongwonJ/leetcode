b56a3823c881a671ca6463c8dbf36713869daa22
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-06-05 01:36:24 +0900

    Time: 92 ms (76.01%), Space: 16.4 MB (81.24%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.py
@@ -8,7 +8,49 @@ from collections import deque
 
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
+
+        if not root: return False
         
+        Lstack, Rstack = [], []
+        Lx, Rx = root, root
+        
+        def makeLeft(Lstack, node):
+            Lstack.append(node)
+            while node:
+                if node.left: Lstack.append(node.left)
+                node = node.left
+        
+        def makeRight(Rstack, node):
+            Rstack.append(node)
+            while node:
+                if node.right: Rstack.append(node.right)
+                node = node.right
+        
+        def nextLeft(Lstack):
+            prev = Lstack.pop()
+            if prev.right: makeLeft(Lstack, prev.right)
+            return prev
+            
+        def nextRight(Rstack):
+            prev = Rstack.pop()
+            if prev.left: makeRight(Rstack, prev.left)
+            return prev
+        
+        makeLeft(Lstack, Lx)
+        makeRight(Rstack, Rx)
+        
+        a, b = nextLeft(Lstack), nextRight(Rstack)
+        
+        while a.val < b.val:
+            if a.val + b.val == k: return True
+            elif a.val + b.val < k: a = nextLeft(Lstack)
+            else: b = nextRight(Rstack)
+        
+        return False
+        
+                
+        
+'''
         if root is None: return False
         
         dq = deque()
@@ -28,4 +70,5 @@ class Solution:
         for i in set_:
             if 2*(k-i) != k and k-i in set_: return True
         return False
+'''
                 
