ba6cdc2b0b2087dfdd20eb293bb63bc64b2bd399
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-06-07 03:19:16 +0900

    Time: 176 ms (26.10%), Space: 14.2 MB (9.60%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.py
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.py
@@ -1,12 +1,30 @@
 class Solution:
     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
-        cnt  = 0 
-        for i in arr1:
-            sgn = False
-            for j in arr2:
-                if (i - j) <= d and (i - j) >= -d:
-                    sgn = True
-            if sgn == False: cnt += 1
-
+        arr1.sort()
+        arr2.sort()
+        cnt = 0 
+        
+        def findMin(n, arr):
+            a, b = 0, len(arr) - 1
+            while a <= b:
+                c = (a + b) // 2
+                if n - arr[c] == d: return c
+                elif n - arr[c] < d: b = c - 1   
+                else: a = c + 1
+            return a
+            
+        def findMax(n, arr):
+            a, b = 0, len(arr) - 1
+            while a <= b:
+                c = (a + b) // 2
+                if arr[c] - n == d: return c
+                elif arr[c] - n < d: a = c + 1
+                else: b = c - 1
+            return b
+    
+        for i in range(len(arr1)):
+            iMax, iMin = findMax(arr1[i], arr2), findMin(arr1[i], arr2)
+            if iMax < iMin: cnt += 1
+          
         return cnt
             
