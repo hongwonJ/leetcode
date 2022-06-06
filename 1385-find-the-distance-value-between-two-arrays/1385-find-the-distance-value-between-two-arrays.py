f465a7f8dfa78f3dde7aef395b3bec555672de12
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-06-07 03:28:39 +0900

    Time: 120 ms (58.57%), Space: 14 MB (79.45%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.py
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.py
@@ -4,27 +4,18 @@ class Solution:
         arr2.sort()
         cnt = 0 
         
-        def findMin(n, arr):
-            a, b = 0, len(arr) - 1
+        for n in arr1:
+            a, b = 0, len(arr2) - 1
+            sgn = True
             while a <= b:
-                c = (a + b) // 2
-                if n - arr[c] == d: return c
-                elif n - arr[c] < d: b = c - 1   
+                c = (a + b)//2
+                if -d <= n - arr2[c] <= d:
+                    sgn = False
+                    break
+                elif n - arr2[c] < -d: b = c - 1
                 else: a = c + 1
-            return a
-            
-        def findMax(n, arr):
-            a, b = 0, len(arr) - 1
-            while a <= b:
-                c = (a + b) // 2
-                if arr[c] - n == d: return c
-                elif arr[c] - n < d: a = c + 1
-                else: b = c - 1
-            return b
-    
-        for i in range(len(arr1)):
-            iMax, iMin = findMax(arr1[i], arr2), findMin(arr1[i], arr2)
-            if iMax < iMin: cnt += 1
+            if sgn: cnt += 1
           
         return cnt
             
+     
