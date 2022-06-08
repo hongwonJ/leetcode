c6a3a0a6b1de9c9ce78b7fb7cd9927cf388628e9
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-06-08 23:11:55 +0900

    Time: 124 ms (39.86%), Space: 15.4 MB (49.46%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,25 +1,32 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
-        sp, ep = 0, len(nums) - 1
-        smp = self.pSearch(sp, ep, nums, target)
-        emp = smp
-        while smp > 0 and nums[smp-1] == target:
-            smp = self.pSearch(sp, smp - 1, nums, target)
-        sp = smp
-        while emp < len(nums) - 1 and nums[emp+1] == target:
-            emp = self.pSearch(emp + 1, ep, nums, target)
-        ep = emp
-
-        return [sp, ep]
-
-    def pSearch(self, a: int, b: int, nums: List[int], target: float) -> int:
-        while a <= b:
-            c = (a + b) // 2
-            if target < nums[c]: b = c - 1
-            elif nums[c] < target: a = c + 1
-            else: return c
-        return -1
-    
-
-
+        def findMin(nums: List[int], target: int) -> List[int]:
+            min_ = -1
+            a, b = 0, len(nums) - 1
+            while a <= b:
+                c = (a + b) // 2
+                if nums[c] == target: 
+                    min_ = c
+                    b = c - 1
+                elif nums[c] < target: a = c + 1
+                else: b = c - 1
+            return min_
+        
+        def findMax(nums: List[int], target: int) -> List[int]:
+            max_ = -1
+            a, b = 0, len(nums) - 1
+            while a <= b:
+                c = (a + b) // 2
+                if nums[c] == target: 
+                    max_ = c
+                    a = c + 1
+                elif nums[c] < target: a = c + 1
+                else: b = c - 1
+            return max_
+        
+        min_ = findMin(nums, target)
+        if min_ == -1: return [-1, -1]
+        max_ = findMax(nums, target)
+        
+        return [min_, max_]
