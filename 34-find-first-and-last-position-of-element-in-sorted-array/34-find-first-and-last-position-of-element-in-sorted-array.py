08f65e2e0973542dc5bb56cb130245e912afd2e4
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-06-08 23:21:10 +0900

    Time: 161 ms (13.55%), Space: 15.5 MB (9.58%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,32 +1,38 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
-        def findMin(nums: List[int], target: int) -> List[int]:
-            min_ = -1
+        def findP(nums: List[int], target: int) -> List[int]:
             a, b = 0, len(nums) - 1
             while a <= b:
                 c = (a + b) // 2
                 if nums[c] == target: 
-                    min_ = c
-                    b = c - 1
+                    return a, b, c
                 elif nums[c] < target: a = c + 1
                 else: b = c - 1
-            return min_
+            return -1, -1, -1
         
-        def findMax(nums: List[int], target: int) -> List[int]:
-            max_ = -1
-            a, b = 0, len(nums) - 1
-            while a <= b:
-                c = (a + b) // 2
-                if nums[c] == target: 
-                    max_ = c
-                    a = c + 1
-                elif nums[c] < target: a = c + 1
-                else: b = c - 1
-            return max_
+        s,e,m = findP(nums,target)
+        if s == -1: return [-1, -1]        
         
-        min_ = findMin(nums, target)
-        if min_ == -1: return [-1, -1]
-        max_ = findMax(nums, target)
+        min_, max_ = -1, -1
+        a, b = s, m
+        while a <= b:
+            c = (a + b) // 2
+            if nums[c] == target:
+                min_ = c
+                b = c - 1
+            elif nums[c] < target: a = c + 1
+            else: b = c - 1
         
+        a, b = m, e
+        while a <= b:
+            c = (a + b) // 2
+            if nums[c] == target:
+                max_ = c
+                a = c + 1
+            elif nums[c] < target: a = c + 1
+            else: b = c - 1
+                
         return [min_, max_]
+    
+    
