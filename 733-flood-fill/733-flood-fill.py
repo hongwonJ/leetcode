a41531fb6566ee372e272faca286746305285d78
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-06-14 02:57:55 +0900

    Time: 176 ms (5.18%), Space: 14.3 MB (13.44%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,28 +1,15 @@
 class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
-        
-        m, n = len(image), len(image[0])
-        stack = []
-        marked = [False]*(n*m)
-        
-        stack.append((sr,sc))
-        sgn = image[sr][sc]
-        image[sr][sc] = newColor
+    def floodFill(self, image, sr, sc, newColor):
+        R, C = len(image), len(image[0])
+        color = image[sr][sc]
+        if color == newColor: return image
+        def dfs(r, c):
+            if image[r][c] == color:
+                image[r][c] = newColor
+                if r >= 1: dfs(r-1, c)
+                if r+1 < R: dfs(r+1, c)
+                if c >= 1: dfs(r, c-1)
+                if c+1 < C: dfs(r, c+1)
 
-        
-        while stack:
-            r, c = stack.pop()
-            marked[r*n + c] = True
-            Adj = []
-            if r-1 >= 0: Adj.append((r-1,c))
-            if r+1 < m: Adj.append((r+1,c))
-            if c-1 >= 0: Adj.append((r,c-1))
-            if c+1 < n: Adj.append((r,c+1))
-            
-            for adjr, adjc in Adj:
-                if not marked[adjr*n + adjc] and image[adjr][adjc] == sgn:     
-                    image[adjr][adjc] = newColor
-                    stack.append((adjr, adjc))
-                marked[adjr*n + adjc] = True
-                        
+        dfs(sr, sc)
         return image
