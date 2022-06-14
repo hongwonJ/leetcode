5f9b021dae4d1f4ac7b9e7d739b392d5abaf0565
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-06-15 02:15:32 +0900

    Time: 338 ms (5.55%), Space: 18.5 MB (5.15%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.py
+++ 695-max-area-of-island/695-max-area-of-island.py
@@ -2,30 +2,21 @@ class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
         max_ = 0
-        marked = [False] * (m * n)
+        seen = set()
+        
+        def area(i,j):
+            if (i,j) in seen: return 0
+            seen.add((i,j))
+            if 0 <= i < m and 0 <= j < n and grid[i][j] == 1:
+                return 1 + area(i+1, j) + area(i-1, j) + area(i, j-1) + area(i, j+1)
+            else:
+                return 0
+            return 0
+           
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1 and not marked[i * n + j]:
-                    con = self.DFS(grid, i, j, marked)
+                if grid[i][j] == 1 and (i,j) not in seen:
+                    con = area(i, j)
                     if  con > max_: max_ = con
+                        
         return max_
-    
-    def DFS(self, grid: List[List[int]], i:int, j:int, marked: List[int]) -> int:    
-        marked[i*len(grid[0]) + j] = True
-        if grid[i][j] == 1:
-            con = 1
-            if i > 0 and not marked[(i-1)*len(grid[0]) + j]:
-                con += self.DFS(grid, i-1, j, marked)
-            if i < len(grid)-1 and not marked[(i+1)*len(grid[0]) + j]:
-                con += self.DFS(grid, i+1, j, marked)
-            if j > 0 and not marked[i*len(grid[0]) + j-1]:
-                con += self.DFS(grid, i, j-1, marked)
-            if j < len(grid[0])-1 and not marked[i*len(grid[0]) + j+1]:
-                con += self.DFS(grid, i, j+1, marked)    
-            return con
-        else:
-            return 0
-            
-        
-            
-        
