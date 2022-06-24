2304e36984128d499f47ce236993287094eb3076
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-06-24 22:45:49 +0900

    Time: 197 ms (10.86%), Space: 14.7 MB (79.51%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -1,15 +1,17 @@
+from collections import deque
 class Solution:
-    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int):
         m, n = len(mat), len(mat[0])
-        if r * c != m * n: return mat
-        res = []
-
-        for i in range(m*n):
-            s, t = divmod(i, c)
-            u, v = divmod(i, n)
-            if t == 0: res.append([])
-            res[s].append(mat[u][v])
-
-        return res
-                
+        if m*n != r*c: return mat
         
+        dq = deque()
+        for i in range(m):
+            for j in range(n):
+                dq.append(mat[i][j])
+        ans = []
+        for i in range(r):
+            ans.append([])
+            for j in range(c):
+                ans[i].append(dq.popleft())
+        return ans
+            
