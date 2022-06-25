0fd92c8f53feac9ea32952dd7032ff8672d37ea6
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-06-25 13:32:21 +0900

    Time: 167 ms (30.23%), Space: 15.7 MB (46.64%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -1,32 +1,16 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        n = len(graph)
-        m = (n - 1)**2 + 1
-        Adj = dict()
+        paths, path = [], []
         
-        Adj[0] = graph[0]
-        for lv in range(n - 2):
-            for nd in range(1, n):
-                if graph[nd]: Adj[lv*15 + nd] = [(lv + 1)*15 + k for k in graph[nd]]
+        def dfs(node):
+            path.append(node)
+            if node == len(graph) - 1:
+                paths.append(path.copy())
+                return
+            
+            for next_node in graph[node]:
+                dfs(next_node)
+                path.pop()
+        dfs(0)
+        return paths
         
-        paths = [[[0]]]
-        for lv in range(n):
-            paths.append([])
-            for path in paths[-2]:
-                if path[-1] in Adj:
-                    for adj_x in Adj[path[-1]]:
-                        n_path = path + [adj_x]
-                        paths[-1].append(n_path)
-        
-        ans = []
-        
-        for lvpaths in paths:
-            for path in lvpaths:
-                if path[-1] % 15 == n-1:
-                    path = [x % 15 for x in path]
-                    ans.append(path)
-
-        return ans
-                    
-                
-   
