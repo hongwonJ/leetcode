9b0c64988548a00c025654555af06a356ee9ce64
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-06-27 08:38:43 +0900

    Time: 111 ms (55.11%), Space: 17.6 MB (48.89%) - LeetHub

---
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -0,0 +1,33 @@
+import heapq as hq
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        flist, blist = [], []
+        for interval in intervals:
+            flist.append(interval[0])
+            blist.append(interval[1])
+        flist.append(float(inf))
+        blist.append(float(inf))
+        
+        hq.heapify(flist)
+        hq.heapify(blist)
+        
+        f, b = hq.heappop(flist), hq.heappop(blist)
+        
+        max_cnt, cnt = 1, 0
+        
+        while flist:
+            if f < b:
+                cnt += 1
+                if cnt > max_cnt: max_cnt = cnt
+                f = hq.heappop(flist)
+            else:
+                cnt -= 1
+                b = hq.heappop(blist)
+            
+        return max_cnt
+            
+            
+        
+        
+        
