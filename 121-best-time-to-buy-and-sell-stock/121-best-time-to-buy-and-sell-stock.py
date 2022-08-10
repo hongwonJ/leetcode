caba2325b6d43dd2f9532e2955213d76c63709fe
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-08-10 16:52:49 +0900

    Time: 1222 ms (80.33%), Space: 26 MB (6.82%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,23 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        A = [0 for _ in range(n)]
-        M = [0 for _ in range(n)]
-        for i in range(n-1, -1, -1):
-            if i == n-1: M[i] = prices[i]
-            else: 
-                if prices[i] > M[i+1]:
-                    M[i] = prices[i]
+        A, M = [0], [0]
+        max_ = 0
+        for i in range(1, n):
+            if i == 1:
+                M.append(prices[i-1])
+                if prices[i] > M[i]:
+                    A.append(prices[i] - M[i])
                 else:
-                    M[i] = M[i+1]
-                if prices[i] < M[i]:
-                    A[i] = M[i] - prices[i]
-        return max(A)
+                    A.append(0)
+            else: 
+                # Setting list M
+                if prices[i-1] < M[i-1]: M.append(prices[i-1])
+                else: M.append(M[i-1])
+                # Setting list A
+                if prices[i] > M[i]: A.append(prices[i] - M[i])
+                else: A.append(0)
+            # find Maximum
+            if A[i] > max_: max_ = A[i]
+        return max_
                 
-        
-            
