1c5063554756ae6142401f6d971aca1873571f92
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-08-21 04:09:01 +0900

    Time: 98 ms, Memory: 13.9 MB - LeetHub

---
+++ roman-to-integer/roman-to-integer.py
@@ -0,0 +1,16 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romDict = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        n = len(s)
+        ans = 0
+        for i in range(n):
+            if s[i] == "I" and i + 1 < n and (s[i+1] == "V" or s[i+1] == "X"): 
+                ans -= 1
+            elif s[i] == "X" and i + 1 < n and (s[i+1] == "L" or s[i+1] == "C"): 
+                ans -= 10
+            elif s[i] == "C" and i + 1 < n and (s[i+1] == "D" or s[i+1] == "M"):
+                ans -= 100
+            else:
+                ans += romDict[s[i]]
+
+        return ans
