ecaa6b1ab2007a931f46231999bcee3f96693f40
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-08-20 12:35:54 +0900

    Time: 391 ms (5.04%), Space: 17.6 MB (74.39%) - LeetHub

---
+++ 384-shuffle-an-array/384-shuffle-an-array.py
@@ -0,0 +1,20 @@
+import random
+
+class Solution:
+    def __init__(self, nums: List[int]):
+        self.origin = nums
+        self.shuffled = nums[:]
+
+    def reset(self) -> List[int]:
+        return self.origin
+        
+    def shuffle(self) -> List[int]:
+        random.seed()
+        random.shuffle(self.shuffled)
+        return self.shuffled
+        
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
