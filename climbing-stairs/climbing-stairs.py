30a17e42b568551079b54d6f923b54d40d850b31
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-08-20 10:56:42 +0900

    Time: 28 ms, Memory: 13.8 MB - LeetHub

---
+++ climbing-stairs/climbing-stairs.py
@@ -0,0 +1,10 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        x = [None for _ in range(n+1)]
+        x[0] = x[1] = 1
+        for i in range(2, n+1):
+            x[i] = x[i-1] + x[i-2]
+        return x[n]
+            
+        
+            
