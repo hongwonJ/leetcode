331f4b296155cd0928b745e2efca1dab55b888cd
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-09-25 05:09:29 +0900

    Time: 1145 ms (17.97%), Space: 0B (100.00%) - LeetHub

--- 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
+++ 1398-customers-who-bought-products-a-and-b-but-not-c/1398-customers-who-bought-products-a-and-b-but-not-c.sql
@@ -1,12 +1,6 @@
 # Write your MySQL query statement below
 SELECT c.customer_id, customer_name
 FROM Customers c LEFT JOIN Orders o ON c.customer_id = o.customer_id
-WHERE c.customer_id IN (
-    SELECT customer_id FROM Orders WHERE product_name = 'A'
-) AND c.customer_id IN (
-    SELECT customer_id FROM Orders WHERE product_name = 'B'
-) AND c.customer_id NOT IN (
-    SELECT customer_id FROM Orders WHERE product_name = 'C'
-)
 GROUP BY c.customer_id
+HAVING SUM(product_name='A') > 0 AND SUM(product_name='B') > 0 AND SUM(product_name='C') = 0
 
