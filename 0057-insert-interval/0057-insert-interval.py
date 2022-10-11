94fde94b130c2796d026cffdef67abb4fd521723
nuWonee <100735853+nuWonee@users.noreply.github.com>
2022-10-11 01:12:51 -0500

    Time: 184 ms (27.20%), Space: 18 MB (6.16%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.py
@@ -0,0 +1,35 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        startset = dict()
+        min_, max_ = float('inf'), float('-inf')
+        for interval in intervals:
+            if interval[0] not in startset or interval[1] > startset[interval[0]]:
+                startset[interval[0]] = interval[1]
+                if interval[0] < min_: min_ = interval[0]
+                if interval[1] > max_: max_ = interval[1]    
+
+        n = min_
+        ans = []
+        while n <= max_: 
+            if n in startset:
+                k, endpoint = n+1, startset[n]
+                while k < endpoint + 1:
+                    if k in startset and startset[k] > endpoint:
+                        endpoint = startset[k]
+                        overlap = True
+                    k += 1
+                ans.append([n, endpoint])
+                n = endpoint + 1
+            else: n += 1
+
+        return ans
+                
+                        
+            
+            
+            
+            
+            
+            
+                
